b92b34c5a89d66115f22d9ed0dd905a2be8544d8
Vinay <vinaykr0605@gmail.com>
2025-06-17 09:55:30 +0000

    removed unwanted files

--- todo-list-app/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todosReducer from './components/todosSlice';
-
-export const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-  },
-});
